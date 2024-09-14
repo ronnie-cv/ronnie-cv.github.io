4d063ffd8d583d01841ca401d5c8369c4149d960
RCV <89815330+ronnie-cv@users.noreply.github.com>
2024-09-14 00:36:48 -0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,12 +34,12 @@ const skillsHeader = document.querySelectorAll('.skills__header');
 
 function toggleSkills() {
     // Toggle between adding and removing the "skills__open" and "skills__close" class
-    if(this.parentNode.classList.contains('skills__close')){
-        this.parentNode.classList.remove('skills_close');
-        this.parentNode.classList.add('skills__open');
-    } else {
+    if(this.parentNode.classList.contains('skills_open')){
         this.parentNode.classList.remove('skills_open');
         this.parentNode.classList.add('skills__close');
+    } else {
+        this.parentNode.classList.remove('skills_close');
+        this.parentNode.classList.add('skills__open');
     }
 }
 
